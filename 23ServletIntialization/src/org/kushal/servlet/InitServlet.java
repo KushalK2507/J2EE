c995b179492680e2e5789f2c41136390edd6ff92
KUSHAL <KUSHAL@KUSHAL-PC>
2019-08-24 19:59:30 +0530

    Servlet Initialization

---
+++ 23ServletIntialization/src/org/kushal/servlet/InitServlet.java
@@ -0,0 +1,50 @@
+package org.kushal.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InitServlet
+ */
+@WebServlet(urlPatterns = { "/InitServlet" }, initParams = {
+		@WebInitParam(name = "message2", value = "Initialization from Annotation", description = "Initialization from Annotation") })
+public class InitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public InitServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ServletConfig config = getServletConfig();
+		String message = config.getInitParameter("message2");
+		response.getWriter().print(message);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
